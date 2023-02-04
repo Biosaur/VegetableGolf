4d4acfa297cbe97612de14255b4b1d2c14e95023
Biosaur <biosaur99@gmail.com>
2023-02-04 14:41:18 -0500

    yes

---
+++ Source/VegetableGolf/VegetableGolfGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VegetableGolfGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEGETABLEGOLF_API AVegetableGolfGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
